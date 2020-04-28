48753f88fdcb6cc16b8e6e73a0e1a2f50eadc792
Iyosayi <Iyosa14@gmail.com>
2020-04-28 10:19:21 +0100

    minor changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // NPM MODULES
 // import the packages we need
-const express = require('express'); // express for routing
-const bodyParser = require('body-parser');
+const express = require("express"); // express for routing
+const bodyParser = require("body-parser");
 
 // CONFIGURE APP
 const app = express();
@@ -15,22 +15,22 @@ const port = process.env.PORT || 4000; // set our port
 
 // BASIC ROUTING
 
-app.get('/', function (_, res) {
-  res.send('Welcome to our API');
+app.get("/", function (_, res) {
+  res.send("Welcome to our API");
 });
 
 // START THE SERVER
 // =============================================================================
 
-if(!module.parent){ 
+if (!module.parent) {
   // Only listen when started directly
   // Without this check, mocha re-listens in watch mode when changes are made
   // This means that it will throw EADDRINUSE err because it is using the port already
   // Learn more http://www.marcusoft.net/2015/10/eaddrinuse-when-watching-tests-with-mocha-and-supertest.html
   app.listen(port);
-  console.log('Magic happens on port ' + port);
- }
+  console.log("Magic happens on port " + port);
+}
 
- // EXPORT APP
- // Export app for test
+// EXPORT APP
+// Export app for test
 module.exports = app;
--- test/home.js
+++ test/home.js
@@ -1,29 +1,29 @@
 process.env.PORT = 4500;
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 
-const server = require('../index');
+const server = require("../index");
 
 chai.should();
 chai.use(chaiHttp);
 
-describe('HOME', () => {
-  describe('GET /', () => {
+describe("HOME", () => {
+  describe("GET /", () => {
     let res = undefined;
     before(async () => {
-      res = await chai.request(server).get('/');
+      res = await chai.request(server).get("/");
     });
 
-    it('Should get home page', () => {
+    it("Should get home page", () => {
       res.should.have.status(200);
     });
-    it('Should have a greeting string', () => {
-      res.text.should.be.a('string');
+    it("Should have a greeting string", () => {
+      res.text.should.be.a("string");
     });
-    it('Should be a welcome greeting=', () => {
-      res.text.should.include.string('Welcome to our API');
+    it("Should be a welcome greeting=", () => {
+      res.text.should.include.string("Welcome to our API");
     });
   });
 });
